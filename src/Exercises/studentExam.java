3885369d58ae57cc50e45b0528106344c146f407
ikemdinachi-alfred <144817846+ikemdinachi-alfred@users.noreply.github.com>
2023-09-12 18:01:50 +0100

    Update studentExam.java

--- src/Exercises/studentExam.java
+++ src/Exercises/studentExam.java
@@ -8,13 +8,16 @@ public class studentExam {
 
         System.out.println(" Answer the following maths Questions");
 
-        System.out.println(" Q1. 100 - 75 = : ");
+        System.out.print(" Q1. 100 - 75 = : ");
         int number= input.nextInt(); if (number==25) System.out.println("Correct");
         if (number!=25) System.out.println("incorrect Answer");
-        System.out.println(" Q2. what is the square root of 5? = : ");
+        System.out.print(" Q2. what is the square root of 5? = : ");
         int number1= input.nextInt();if (number1==25) System.out.println("Correct");
         if (number1!=25) System.out.println("incorrect Answer");
-
+        System.out.print(" Q3. 25 + 45 = : "); 
+        int number2 = input.nextInt(); if(number2==70) System.out.println("correct");
+        if(number2!=70) System.out.println("incorrect Answer");
+        
         System.out.println("well done, Thanks for your Time ");
     }
 }
